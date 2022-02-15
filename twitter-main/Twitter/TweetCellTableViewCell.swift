e5882da8084a1449d15600efed5a5fb37bddbc39
Muhammad Jawwad <mjawwad2@horizon.csueastbay.edu>
2022-02-14 20:50:41 -0800

    Twitter CodePath
    This is my final project for CodePath Unit 3

---
+++ twitter-main/Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Muhammad Jawwad on 2/14/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNamelabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
